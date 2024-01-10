1a2fe3b323a83db6eb7814368e5d0b56451e274a
lai2-24 <lai2-24@itismeucci.com>
2024-01-10 09:30:11 +0100

    first commit

---
+++ src/main/java/meucci/tpsit/App.java
@@ -0,0 +1,13 @@
+package meucci.tpsit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
